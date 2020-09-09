23df0bb5b0ffaca6e09272d6e53d6db4e753268f
hedy <80673139@qq.com>
2020-09-09 14:36:02 +0800

    1

--- src/InterfaceDemo4/SXHardDisk.java
+++ src/InterfaceDemo4/SXHardDisk.java
@@ -1,6 +1,8 @@
 package InterfaceDemo4;
 
-public class SXHardDisk implements HardDisk {
+public class
+
+SXHardDisk implements HardDisk {
     @Override
     public String getVolumn() {
         return "三星Disk";
